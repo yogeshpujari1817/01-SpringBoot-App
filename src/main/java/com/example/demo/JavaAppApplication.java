69b817185700b911745a3bd15925bbd372f27196
Container_Coder <yogeshpujari1817@gmail.com>
2024-01-02 14:50:25 +0530

    HIS 310 Changes

--- src/main/java/com/example/demo/JavaAppApplication.java
+++ src/main/java/com/example/demo/JavaAppApplication.java
@@ -9,12 +9,19 @@ public class JavaAppApplication {
 
 	public static void main(String[] args) {
 		
+		private Map<String,object> cache = new HashMap<String,object>();
+		
 		ConfigurableApplicationContext run = SpringApplication.run(JavaAppApplication.class, args);
 		
 		int i =10;
 		
 		//changes for HIS-134
 		run.close();
+		
+		//HIS-301 changes
+		public void loadDataToCache(){
+			//logic
+		}
 	}
 
 	
